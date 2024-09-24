4c71ab8a3c12b0ea1d53654c53b8236f8bc04cf1
Alena Chaikouskaya <alec@equinor.com>
2024-09-24 13:30:38 +0200

    Add testcase with wait time

---
+++ tests/performance/script-random-inlineslice-wait.js
@@ -0,0 +1,29 @@
+import { sendSetupMetadataRequest } from "./helpers/metadata-helpers.js";
+import { sendRandomSliceRequest } from "./helpers/slice-helpers.js";
+import { createSummary, thresholds, summaryTrendStats } from "./helpers/report-helpers.js";
+import { sleep } from "k6";
+
+export const options = {
+  scenarios: {
+    randomInlineSliceWait: {
+      executor: "constant-vus",
+      vus: 1,
+      duration: "50m",
+    },
+  },
+  thresholds: thresholds(),
+  summaryTrendStats: summaryTrendStats(),
+};
+
+export function setup() {
+  return sendSetupMetadataRequest();
+}
+
+export default function (metadata) {
+  sendRandomSliceRequest(metadata, "Inline");
+  sleep(200);
+}
+
+export function handleSummary(data) {
+  return createSummary(data);
+}
